a56dcd6f3f8e01f2decf769bd51214e0581a6fc5
anilbhaila <anil.bhaila.2020@gmail.com>
2026-02-16 18:44:06 +0000

    added code to work with duckdb as well

--- 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -1,30 +1,44 @@
-select 
-    --Identifiers
-    cast(vendorid as integer) as vendor_id,
-    cast(ratecodeid as integer) as rate_code_id,
-    cast(pulocationid as integer) as pickup_location_id,
-    cast(dolocationid as integer) as dropoff_location_id,
-    
-    --Timestamps
-    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
-    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
-    
-    --Trip info
-    cast(store_and_fwd_flag as string) as store_and_fwd_flag,
-    cast(passenger_count as integer) as passenger_count,
-    cast(trip_distance as numeric) as trip_distance,
-    01 as trip_type, -- yellow taxi can only be Street-hail, so we can hardcode this value
-    
-    --Payment details
-    cast(fare_amount as numeric) as fare_amount,
-    cast(extra as numeric) as extra,
-    cast(mta_tax as numeric) as mta_tax,
-    cast(tip_amount as numeric) as tip_amount,
-    cast(tolls_amount as numeric) as tolls_amount,
-    cast(improvement_surcharge as numeric) as improvement_surcharge,
-    0 as ehail_fee, -- yellow taxi does not have an e-hail fee, so we can hardcode this value
-    cast(total_amount as numeric) as total_amount,
-    cast(payment_type as integer) as payment_type
+with source as(
+    select * from {{ source('raw_data', 'yellow_tripdata') }}
+),
 
-from {{ source('raw_data', 'yellow_tripdata') }}
-where vendorid is not null
+renamed as (
+    select 
+        --Identifiers
+        cast(vendorid as integer) as vendor_id,
+        cast(ratecodeid as integer) as rate_code_id,
+        cast(pulocationid as integer) as pickup_location_id,
+        cast(dolocationid as integer) as dropoff_location_id,
+        
+        --Timestamps
+        cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+        cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
+        
+        --Trip info
+        cast(store_and_fwd_flag as string) as store_and_fwd_flag,
+        cast(passenger_count as integer) as passenger_count,
+        cast(trip_distance as numeric) as trip_distance,
+        01 as trip_type, -- yellow taxi can only be Street-hail, so we can hardcode this value
+        
+        --Payment details
+        cast(fare_amount as numeric) as fare_amount,
+        cast(extra as numeric) as extra,
+        cast(mta_tax as numeric) as mta_tax,
+        cast(tip_amount as numeric) as tip_amount,
+        cast(tolls_amount as numeric) as tolls_amount,
+        cast(improvement_surcharge as numeric) as improvement_surcharge,
+        0 as ehail_fee, -- yellow taxi does not have an e-hail fee, so we can hardcode this value
+        cast(total_amount as numeric) as total_amount,
+        cast(payment_type as integer) as payment_type
+
+    from {{ source('raw_data', 'yellow_tripdata') }}
+    -- Filter out records with null vendor_id (data quality requirement)
+    where vendorid is not null
+)
+
+select * from renamed
+
+-- Sample records for dev environment using deterministic date filter
+{% if target.name == 'dev' %}
+where pickup_datetime >= '2019-01-01' and pickup_datetime < '2019-02-01'
+{% endif %}
